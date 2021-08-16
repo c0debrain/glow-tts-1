4a3360a1012f07f2d8da9c40eaafb7b83acbd1d8
shahin <codebrain@gmail.com>
2021-08-16 14:54:22 +1000

    update symbols

--- text/symbols.py
+++ text/symbols.py
@@ -12,13 +12,13 @@ _eos = '~'
 _special = '-'
 _english_jaso_code = list(range(0x0040,0x007A))
 _bengali_jaso_code = list(range(0x0980,0x09FF))
-_jaso_code = _english_jaso_code + _bengali_jaso_code
+_jaso_code = _bengali_jaso_code
 _jaso = list(chr(c) for c in _jaso_code)
 _letters = _jaso
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
-_arpabet = ['@' + s for s in cmudict.valid_symbols]
+#_arpabet = ['@' + s for s in cmudict.valid_symbols]
 
 # Export all symbols:
 #symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
-symbols = [_pad] + list(_punctuation) + list(_punctuation) + list(_letters) + _arpabet
+symbols = [_pad] + list(_punctuation) + list(_letters)
